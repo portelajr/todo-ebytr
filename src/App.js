b92d36ed314e21327ea20e3d22da102511bce1a8
Junior Portela <portelajr14@gmail.com>
2021-11-05 15:30:43 -0300

    Estrutura e planejamento inicial do projeto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="headerList">
+        <p>Lista de tarefas</p>
       </header>
+      <div className="taskList">
+        <p> FUNÇÃO QUE RENDERIZA DADOS PERCORRIDOS</p>
+        <p> RENDERIZAÇÃO DE COMPONENTES COM INFOS PROPS.CHILDREN </p>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+// listagem de tarefas tem que ser armazenada em algum local, após o reload.
+// lista armazenada vai ser renderizada por uma função (iteração)
+// tarefas devem ter tag's de identificação
+// tags (como? pensar)
+// o sort opicional na renderização vai ocorrer conforme tags
+// tags como atributos (?)
+// taks's como componentes recebendo a lista por props.children
+
+
+// REQUISITOS:
+//Funcionalidades:
+// Visualizar a lista de tarefas;
+// Esta lista deve ser ordenável por ordem alfabética, data de criação ou por status;
+// Inserir uma nova tarefa na lista;
+// Remover uma tarefa da lista;
+// Atualizar uma tarefa da lista;
+// A tarefa deve possuir um status editável: pendente, em andamento ou pronto;
---
+++ src/components/TaskList.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class TaskList extends React.Component {
+  render() {
+    return (
+      <div>
+        <p> componente componente componente</p>
+      </div>
+    )
+  }
+}
+
+export default TaskList;
